f7878fe440b518b1ec56d3d8e468cc17495cbe8b
kscarrot <kelstrait@icloud.com>
2025-05-22 14:06:15 +0800

    feat: 集成 Vite-react 并重构项目结构 - 升级node版本到node24 - 添加 Vite 开发服务器支持 - 将 React 配置至 visual 目录

---
+++ src/visual/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello React + SWC + Vite!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  root: 'src/visual',
+  resolve: {
+    alias: {
+      src: resolve(__dirname, './src'),
+    },
+  },
+})
