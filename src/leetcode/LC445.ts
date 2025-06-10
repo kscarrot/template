6bdcc1769d40a1ce176fe90b3f87a2e67c1179b3
kscarrot <kelstrait@icloud.com>
2025-06-10 14:52:33 +0800

    fix: 修正错误的题号文件名

--- src/leetcode/LC445.ts
+++ src/leetcode/LC445.ts
@@ -3,8 +3,10 @@ import addTwoNumbersInverse from './LC2'
 import reverseList from './LC206'
 
 /**
+ * @name: 两数相加 II
+ * @level: 中等
  * @link: https://leetcode.cn/problems/add-two-numbers-ii
- * @description: 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表
+ * @description: 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
  * @param {ListNode | null} l1
  * @param {ListNode | null} l2
  * @return {ListNode | null}
